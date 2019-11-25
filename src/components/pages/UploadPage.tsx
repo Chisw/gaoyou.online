9d8584ea1a6211a61c1e2b254b9becc0d6aa5fea
Chisw <chisw@qq.com>
2019-11-25 22:22:57 +0800

    change favicon, add file input

--- src/components/pages/UploadPage.tsx
+++ src/components/pages/UploadPage.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Body from '../layout/Body'
 import Center from '../layout/Center'
+import { FileInput } from '@blueprintjs/core'
 
 export default function UploadPage() {
   return (
     <Body>
       <Center>
-        Upload
+        <div className="py-6 w-640px">
+          <div className="p-4 bg-gray-800 rounded">
+            <div className="mb-4 h-160 border-2 border-dashed border-gray-600 rounded-lg cursor-pointer hover:border-gray-500"></div>
+            <FileInput
+              fill
+            />
+          </div>
+        </div>
       </Center>
     </Body>
   )
