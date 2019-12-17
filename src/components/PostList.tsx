7a8f74adc9ec2427c941b2b17d7bba7bd0cdc3d1
Chisw <chisw@qq.com>
2019-12-17 11:20:07 +0800

    use gestalt Masonry

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { IPost } from '../ts/types'
 import PostCard from './PostCard'
+import { Masonry } from 'gestalt'
+import 'gestalt/dist/gestalt.css'
 
 interface PostListProps {
   posts: IPost[]
@@ -13,12 +15,12 @@ export default function PostList(props: PostListProps) {
   } = props
 
   return (
-    <div className="htc-post-list -mx-2">
-      {
-        posts.map((post, index) => {
-          return <PostCard key={index} post={post} />
-        })
-      }
+    <div className="htc-post-list">
+      <Masonry
+        comp={(data: any, index) => <PostCard post={data.data} key={index} />}
+        items={posts}
+        minCols={1}
+      />
     </div>
   )
 }
